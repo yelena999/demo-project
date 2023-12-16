1f7bc2ebc1d6764d03c439f086b14e334940f775
feyruzshahmuradov <feyruz@loopcamp.io>
2023-12-16 14:54:31 -0500

    github trigger set up

--- src/test/java/loopcamp/StepDefs.java
+++ src/test/java/loopcamp/StepDefs.java
@@ -46,7 +46,7 @@ public class StepDefs {
         Thread.sleep(2000);
 //        Assert.assertTrue(Driver.getDriver().findElement(By.cssSelector("span[class^='a-color-state']")).getText().contains("useless box"));
 //        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
-        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//h1[@class='srp-controls__count-heading']//span)[2]")).getText().contains("useless boxxx"));
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//h1[@class='srp-controls__count-heading']//span)[2]")).getText().contains("useless box"));
 
     }
 
