35b9feeeb9333fdcfdbd9d778c22e643bf67162b
feyruzshahmuradov <feyruz@loopcamp.io>
2023-12-20 19:57:04 -0500

    github trigger set up

--- src/test/java/loopcamp/Driver.java
+++ src/test/java/loopcamp/Driver.java
@@ -39,7 +39,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "3.92.199.191";
+                        String gridAddress = "54.226.36.175";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
@@ -52,7 +52,7 @@ public class Driver {
                 case "remote-firefox":
                     try {
                         // assign your grid server address
-                        String gridAddress = "3.92.199.191";
+                        String gridAddress = "54.226.36.175";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
